87f449bc4e21561e67d86f4dd7096724ce06b3d9
Lilly-27 <ghcecilia1@gmail.com>
2022-05-23 21:09:29 -0500

    setting up events

---
+++ controllers/eventController.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const Event = require('../models/eventModels')
+
+const Router = express.Router();
+
+// Display all events
+
+// Create an event
+
+// Update an event 
+
+// Delete an event
