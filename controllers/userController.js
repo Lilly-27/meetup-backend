933f7a9f4e96cfcd711c2422f77845d8e94b7267
tclarke94 <loganclarke@Logans-Air.hsd1.co.comcast.net>
2022-05-24 19:34:44 -0600

    Routes completed and nested schemas

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,22 +10,22 @@ router.get('/', (req,res) => {
     )
 });
 
-router.get('/', (req,res) => {
-    User.find({}).then((data)=>
-    res.json(data)
-    )
-});
+
 //Add new user
 router.post('/', (req,res)=> {
-    User.create(req.body).then(() => res.redirect('/'))  
+    User.create(req.body).then(() => res.redirect('/users'))  
 })
 
 
 //Update user
-router.put('/users/:id', (req,res)=> {
-    res.json(User.findByIdAndUpdate({_id: req.params.id}, req.body))
-    // .then(items => res.redirect('/'))
-    
+router.put('/:id', async (req,res)=> {
+    console.log(req.body)
+    res.json(await User.findByIdAndUpdate(req.params.id, req.body))
+     
 })
 
+//Delete user
+router.delete('/:id', async (req, res)=>{
+    res.json(await User.findByIdAndRemove(req.params.id))
+})
 module.exports = router
