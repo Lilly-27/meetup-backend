5f6217dd3aa62ffe2814807c23b43c835b544aaa
tclarke94 <loganclarke@Logans-Air.hsd1.co.comcast.net>
2022-05-31 19:30:22 -0600

    added tags

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -37,4 +37,25 @@ router.get('/:id', (req,res) => {
     res.json(data)
     )
 });
+
+//Find by a field
+router.get('/coffeechats', (req,res)=>{
+    Event.find({tag:'coffee_chats'}).then((data)=>
+    res.json(data)
+    )
+})
+
+router.get('/socials', (req,res)=>{
+    Event.find({tag:'socials'}).then((data)=>
+    res.json(data)
+    )
+})
+
+router.get('/codingsessions', (req,res)=>{
+    Event.find({tag:'coding_sessions'}).then((data)=>
+    res.json(data)
+    )
+})
+
+
 module.exports = router
