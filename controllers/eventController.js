fe7589f689d35cabc57ee5cacd3e3a873a0ca1af
tclarke94 <loganclarke@Logans-Air.hsd1.co.comcast.net>
2022-06-01 17:34:50 -0600

    tag update

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -46,19 +46,19 @@ router.get('display/:id', (req,res) => {
 
 //Find by a field
 router.get('/coffeechats', (req,res)=>{
-    Event.find({tag:'coffee_chats'}),(err, event).then((data)=>
+    Event.find({'tag':'coffee_chats'}),(err, event).then((data)=>
     res.json(data)
     )
 })
 
 router.get('/socials', (req,res)=>{
-    Event.find({tag:'socials'}),(err, event).then((data)=>
+    Event.find({'tag':'socials'}),(err, event).then((data)=>
     res.json(data)
     )
 })
 
 router.get('/codingsessions', (req,res)=>{
-    Event.find({tag:'coding_sessions'}),(err, event).then((data)=>
+    Event.find({'tag':'coding_sessions'}),(err, event).then((data)=>
     res.json(data)
     )
 })
