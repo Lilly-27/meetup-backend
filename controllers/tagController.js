5f2301667f148d89b289fb12b46b8de2fbebbbf7
tclarke94 <loganclarke@Logans-Air.hsd1.co.comcast.net>
2022-06-01 17:47:29 -0600

    tag

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -6,19 +6,19 @@ const router = express.Router();
 
 //Find by a field
 router.get('/coffeechats', (req,res)=>{
-    Event.find({'tag':'coffee_chats'}),(err, event).then((data)=>
+    Event.find({'tag':'coffee_chats'}).then((data)=>
     res.json(data)
     )
 })
 
 router.get('/socials', (req,res)=>{
-    Event.find({'tag':'socials'}),(err, event).then((data)=>
+    Event.find({'tag':'socials'}).then((data)=>
     res.json(data)
     )
 })
 
 router.get('/codingsessions', (req,res)=>{
-    Event.find({'tag':'coding_sessions'}),(err, event).then((data)=>
+    Event.find({'tag':'coding_sessions'}).then((data)=>
     res.json(data)
     )
 })
